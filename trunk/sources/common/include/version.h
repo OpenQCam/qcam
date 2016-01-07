208f6b68d67451b55c8f76dbe0aa0cc4bf0fb183
sjh <s.j.hung@quantatw.com>
2016-01-07 17:13:41 +0800

    add missing files.

---
+++ trunk/sources/common/include/version.h
@@ -0,0 +1,9 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// refer to https://github.com/pmirshad/cmake-with-git-metadata
+#define GIT_BRANCH "ver2"
+#define GIT_COMMIT_HASH "4b69f8c"
+#define GIT_APPLICATION_REVISION ""
+
+#endif
